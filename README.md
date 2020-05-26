310afb51da935797702f9ee606db8070c7953604
Tim Wilson <tim@savvytechgroup.com>
2020-05-26 14:09:54 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# archery-age-division-calculator
+# Archery Age Division Calculator
 Simple calculator for NFAA and USA Archery age divisions based on birthdate and date of competition
+
+## Instructions
+Please consult the `demo-form.html` file for a basic example of building a form that uses
+the date calculations in the `js/alculate-age-divisons.js` file. You'll need to make sure
+assign the appropriate `id` fields so the script will be able to insert the proper values
+into the output table.
+
+The HTML is basic with no frills. You can dress it up however you like as long as you
+maintain the `id`s that the script is expecting.
+
+## Requirements
+This script relies on the third-party `Moment.js` library to do its date calculations. You
+can see how to import that library in the `demo-form.html` file near the top in the `<head>`
+section.
+
+The script is provided for free. Please consider giving credit to Tim Wilson and linking
+back to [archerytoolkit.net](https://archerytoolkit.net/) where you can find other 
+useful archery-related software. A link to the [Minnesota Archers Alliance (MAA)](https://themnaa.org/) 
+would also be appreciated.
---
+++ js/calculate-age-divisions.js
@@ -0,0 +1,90 @@
+function nfaaDivision(age) {
+	if (age < 12) { 
+		return "Cub (under 12)";
+	} else if (age <= 14) {
+		return "Youth (12&ndash;14)";
+	} else if (age <= 17) {
+		return "Young Adult (15&ndash;17)";
+	} else if (age <= 49) {
+		return "Adult (18&ndash;49)";
+	} else if (age <= 59) {
+		return "Senior (50+)";
+	} else if (age <= 69) {
+		return "Silver Senior (60+)";
+	} else {
+		return "Master Senior (70+)";
+	};
+};
+
+function usaDivision(age) {
+	if (age <= 12) { 
+		return "Bowman (under 12)";
+	} else if (age <= 14) {
+		return "Cub (12&ndash;14)";
+	} else if (age <= 17) {
+		return "Cadet (15&ndash;17)";
+	} else if (age <= 20) {
+		return "Junior (18&ndash;20)";
+	} else if (age <= 49) {
+		return "Senior (21&ndash;49)";
+	} else if (age <= 59) {
+		return "Master 50+";
+	} else if (age <= 69) {
+		return "Master 60+";
+	} else {
+		return "Master 70+";
+	};
+};
+
+function calculateAgeDivisions() {
+	var m = document.getElementById('dobMonth');
+	var month = String(m.options[m.selectedIndex].value);
+	var d = document.getElementById('dobDay');
+	var day = String(d.options[d.selectedIndex].value);
+	var y = document.getElementById('dobYear');
+	var year = String(y.options[y.selectedIndex].value);
+	
+	var eventM = document.getElementById('eventMonth');
+	var eventMonth = String(eventM.options[eventM.selectedIndex].value);
+	var eventD = document.getElementById('eventDay');
+	var eventDay = String(eventD.options[eventD.selectedIndex].value);
+	var eventY = document.getElementById('eventYear');
+	var eventYear = String(eventY.options[eventY.selectedIndex].value);			
+
+	if (month != "" && day != "" && year != "") {
+	
+		/* Calculate current age based on DOB */
+		var today = moment();
+		var formDate = month + day + year;
+		var DOB = moment(formDate, 'MMDDYYYY');
+		var age = today.diff(DOB, 'years');
+		
+		/* Calculate age as of Dec 31 for USA Archery purposes */
+		var usaArcheryAge = moment().year() - DOB.year();
+
+		/* Insert the divisions for current age */				
+		document.getElementById('ageNFAA').innerHTML = nfaaDivision(age);
+		document.getElementById('ageUSAArchery').innerHTML = usaDivision(usaArcheryAge);
+		document.getElementById('todayAge').innerHTML = "Today (age " + age + ")";
+		document.getElementById('output').style.display = "table";
+		document.getElementById('ageDivisions').style.display = "table-row";
+	};
+		
+	/* Insert the divisions for a future event date */
+	if (eventMonth != "" && eventDay != "" && eventYear != "") {
+	
+		/* Calculate age at future date */
+		var eventDate = moment(eventYear + '-' + eventMonth + '-' + eventDay);
+		var ageAtEvent = eventDate.diff(DOB, 'years');
+		
+		/* Calculate age as of Dec 31 during the year of the future event. */
+		var usaArcheryEventAge = eventDate.year() - DOB.year();
+		
+		/* Insert the divisions for the future event */
+		document.getElementById('eventNFAA').innerHTML = nfaaDivision(ageAtEvent);
+		document.getElementById('eventUSAArchery').innerHTML = usaDivision(usaArcheryEventAge);
+		document.getElementById('eventDateAndAge').innerHTML = 
+			eventDate.format('MMM D, YYYY') + " (age " + ageAtEvent + ")";
+		document.getElementById('eventDivisions').style.display = "table-row";				
+	};
+};
